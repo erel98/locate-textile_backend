a7fd4e505bff3c6dd5c83372cd9e4b40b6eb67cc
EC2 Default User <ec2-user@ip-172-31-6-178.ec2.internal>
2022-11-30 19:16:31 +0000

    login 2

--- application.py
+++ application.py
@@ -28,10 +28,10 @@ def login():
     user = db.get_an_item(region, 'users', key_info)
     
     if user is None:
-        return jsonify({"msg": "Incorrect login credentials"}), 401
+        return jsonify({"msg": "Incorrect login credentials"})
     else:
         if not bcrypt.checkpw(password, bytes(user['password'])):
-            return jsonify({"msg": "Incorrect login credentials"}), 401
+            return jsonify({"msg": "Incorrect login credentials"})
         
     access_token = create_access_token(identity=email)
     # save to db
