d7b33207f4512a09e5e314119e1ccb261590e77b
EC2 Default User <ec2-user@ip-172-31-6-178.ec2.internal>
2022-12-05 22:43:08 +0000

    wip

--- application.py
+++ application.py
@@ -183,6 +183,6 @@ def updateCoordinate():
     
     
 if __name__ == '__main__':
-    application.run()
-    #application.run(host="0.0.0.0", port="8080")
+    # application.run()
+    application.run(host="0.0.0.0", port="8080")
     
